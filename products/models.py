966d6265b850e708dd22fd8da471af1ae528647a
Vera Leitao <veraleitao.dev@gmail.com>
2021-01-11 11:13:54 +0000

    renamed Category to Collection

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class Collection(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    collection = models.ForeignKey('Collection', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
