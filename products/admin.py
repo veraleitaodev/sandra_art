8fa3d51a581ec75b84e23afddeebd779512924db
Vera Leitao <veraleitao.dev@gmail.com>
2021-01-12 16:11:38 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,27 @@ from .models import Product, Collection
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Collection)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'collection',
+        'price',
+        'image',
+        'image_url',
+        'sold'
+    )
+
+    ordering = ('sku',)
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Collection, CollectionAdmin)
