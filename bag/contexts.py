9ee1bb86ba7475194a7f7b67414a62fe35a65957
Vera Leitao <veraleitao.dev@gmail.com>
2021-01-21 14:23:47 +0000

    updated contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,25 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
     delivery = 0
-
+    bag = request.session.get('bag', {})
     grand_total = delivery + total
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'bag_items': bag_items,
@@ -14,5 +28,6 @@ def bag_contents(request):
         'delivery': delivery,
         'grand_total': grand_total,
     }
+
 
     return context
