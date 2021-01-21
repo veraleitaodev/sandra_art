1fa2357dcc38c53f5e149cc8d66f1a834f021283
Vera Leitao <veraleitao.dev@gmail.com>
2021-01-21 13:39:44 +0000

    add quantity in product detail and add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,4 @@
-from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404
-)
-from django.contrib import messages
-
-from products.models import Product
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -15,32 +10,15 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
+    quantity = 1
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    bag[item_id] = quantity
-    messages.success(request, f'Added {product.name} to your bag')
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
-
-
-def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
-    try:
-        product = get_object_or_404(Product, pk=item_id
-        bag = request.session.get('bag', {})
-
-
-        bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
